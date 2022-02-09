a518f9cd5c1cb079157e2973f5180a570bfdeb87
nurmelekovat <nurmeleqovat@gmail.com>
2022-02-10 00:01:12 +0300

    CRUD tamam

---
+++ src/com/innova/controller/AdminController.java
@@ -0,0 +1,42 @@
+package com.innova.controller;
+
+
+import com.innova.dao.AdminDao;
+import com.innova.dao.IDaoConnection;
+import com.innova.dto.AdminDto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//S.O.L.I.D
+//Single Responsibility
+public class AdminController implements IDaoConnection<AdminDto> {
+
+    private AdminDao adminDao;
+    private AdminDto adminDto;
+    private List<AdminDto> adminList;
+
+    @Override
+    public void create(AdminDto adminDto) {
+        adminDao=new AdminDao();
+        adminDao.create(adminDto);
+    }
+
+    @Override
+    public void update(AdminDto adminDto) {
+        adminDao=new AdminDao();
+        adminDao.update(adminDto);
+    }
+
+    @Override
+    public void delete(AdminDto adminDto) {
+        adminDao=new AdminDao();
+        adminDao.delete(adminDto);
+    }
+
+    @Override
+    public ArrayList<AdminDto> list() {
+        adminDao=new AdminDao();
+        return adminDao.list();
+    }
+}
--- src/com/innova/util/DatabaseConnection.java
+++ src/com/innova/util/DatabaseConnection.java
@@ -1,7 +1,6 @@
 package com.innova.util;
 
 import java.sql.Connection;
-import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
@@ -15,7 +14,7 @@ public class DatabaseConnection extends DatabaseInformation{
     private  String userPassword=this.getUserPassword();
 
     //Singleton Design Pattern
-    private DatabaseConnection instance;
+    private static DatabaseConnection instance;
 
     //private constructor
     private DatabaseConnection() {
@@ -31,7 +30,7 @@ public class DatabaseConnection extends DatabaseInformation{
     }
     //amaçım eğer bağlantı varsa onu kullanmak
     //eğer bağlantı kapatılmış veya bağlantı yoksa instance yapmak
-    public DatabaseConnection getInstance() {
+    public static DatabaseConnection getInstance() {
         try {
             if(instance==null)
                 instance=new DatabaseConnection();
@@ -42,16 +41,16 @@ public class DatabaseConnection extends DatabaseInformation{
         }
         return instance;
     }
-//    public Connection getConnection() {
-//        return connection;
-//    }
-//
-//    public DatabaseConnection setConnection(Connection connection) {
-//        this.connection = connection;
-//        return this;
-//    }
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public DatabaseConnection setConnection(Connection connection) {
+        this.connection = connection;
+        return this;
+    }
 
     public static void main(String[] args) {
-        DatabaseConnection databaseConnection=new DatabaseConnection();
+        //DatabaseConnection databaseConnection=new DatabaseConnection();
     }
 }
